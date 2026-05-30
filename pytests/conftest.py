9df4d5935d166ad31b1dcd0ee7a0e753f795b143
mrava87 <matteoravasi@gmail.com>
2026-05-30 09:34:55 +0100

    ci: fix ptx version in conftest

--- pytests/conftest.py
+++ pytests/conftest.py
@@ -1,5 +1,5 @@
 import os
 
-# Set the default PTX compute capability for Numba to 8.9,
+# Set the default PTX compute capability for Numba to 8.6,
 # currently required by the self-hosted NVIDIA GPU drivers.
-os.environ["NUMBA_CUDA_DEFAULT_PTX_CC"] = "8.9"
+os.environ["NUMBA_CUDA_DEFAULT_PTX_CC"] = "8.6"
